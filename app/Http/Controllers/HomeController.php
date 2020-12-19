7309cd5839b2466034197595c8f87eab8420647a
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-19 20:00:28 +0100

    Index Changed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Controller for Staffs
+ */
 
 class HomeController extends Controller
 {
@@ -23,6 +28,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        return view('home',compact('user'));
     }
 }
