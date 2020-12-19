b9c5f401195f29c3da1e3452d03e1fece7439708
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-19 20:02:03 +0100

    Admin resource route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +22,5 @@ Route::get('/', function () {
 Auth::routes(['verify'=>true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/admin',AdminController::class)->name('*','admin');
