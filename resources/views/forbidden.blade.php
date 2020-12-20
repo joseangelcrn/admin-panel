1b7fdac979ffa00ae6af6603b2a3edd79d4c3905
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-20 16:05:12 +0100

    New forbidden page

---
+++ resources/views/forbidden.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.not-verified')
+
+@section('content')
+ <div class="container card shadow p-5">
+    <div class="row">
+        <div class="col-12 mb-5 text-center">
+            <h1>No tienes permisos para acceder a este sitio </h1>
+            <p class="mt-3">
+                <i class="fas fa-shield-alt fa-10x"></i>
+            </p>
+        </div>
+    </div>
+ </div>
+@endsection
