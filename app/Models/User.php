2122ddba5689aaf7c771ca5577cd5596e0cdaa46
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-20 16:13:35 +0100

    getUsersByRoleName function has been optimized

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -59,11 +60,7 @@ class User extends Authenticatable
 
     public static function getUsersByRoleName($name)
     {
-
-        $usersByRole = User::with('roles')->whereHas('roles', function($q){
-            $q->where('name','staff');
-        })->get();
-
+        $usersByRole = Role::findByName($name)->users;
         return $usersByRole;
     }
 
