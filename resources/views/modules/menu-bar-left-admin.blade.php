af97e65b2f297501743903fed06e2bb33f791b37
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-20 16:58:47 +0100

    splitted role menu

---
+++ resources/views/modules/menu-bar-left-admin.blade.php
@@ -0,0 +1,18 @@
+
+<!-- Heading -->
+<div class="sidebar-heading">
+    admin
+</div>
+
+
+<li class="nav-item active">
+    <a class="nav-link" href="{{route('admin.index')}}">
+        <i class="fas fa-users"></i>
+        <span>Usuarios Staff</span></a>
+</li>
+
+<li class="nav-item active">
+    <a class="nav-link" href="{{route('admin.index')}}">
+        <i class="fas fa-tasks"></i>
+        <span>Tareas sin asignar</span></a>
+</li>
