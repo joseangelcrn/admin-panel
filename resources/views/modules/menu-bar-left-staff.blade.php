5045f19935bed80a534185c50db1babf5fe833c6
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-21 20:25:47 +0100

    Setting staff menu preview

--- resources/views/modules/menu-bar-left-staff.blade.php
+++ resources/views/modules/menu-bar-left-staff.blade.php
@@ -4,10 +4,18 @@
     staff
 </div>
 
-<li class="nav-item active">
-    <a class="nav-link" href="{{route('admin.index')}}">
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-tasks"></i>
-        <span>Mis tareas</span></a>
+        <span>Mis tareas</span>
+    </a>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Submenu</h6>
+            <a class="collapse-item" href="buttons.html">En proceso</a>
+            <a class="collapse-item" href="buttons.html">Terminadas</a>
+        </div>
+    </div>
 </li>
-
-
