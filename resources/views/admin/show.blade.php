35076907dafcff900cb272c1bde9cd707e33a0dc
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-22 19:38:31 +0100

    importing css part of datatable

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link href="{{ asset('js/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row">
@@ -25,53 +29,28 @@
         <div class="row">
             <div class="col-lg-12 col-md-12">
                 <h1 class="my-5">Tareas asignadas</h1>
-                <div class="mx-0 mb-2">
+                <div class="mx-0 mb-3">
                     <a href="#" class="btn btn-success" data-toggle="modal" data-target="#modal-task-assign">Asignar nueva tarea</a>
-                                {{-- Button --}}
-            {{-- <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                Logout
-            </a> --}}
                 </div>
-                <table class="table table-bordered" id="dataTable-tasks" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th class="col-4">Titulo</th>
-                            <th class="col-4">Descripción</th>
-                            <th class="col-4">Opciones</th>
-                        </tr>
-                    </thead>
-                    <tfoot>
-                        <tr>
-                            <th class="col-4">Titulo</th>
-                            <th class="col-4">Descripcion</th>
-                            <th class="col-4">Opciones</th>
-                        </tr>
-                    </tfoot>
-                    <tbody>
-                        @foreach ($user->tasks  as $task)
-                            <tr>
-                                <td>{{$task->title}}</td>
-                                <td>{{Str::limit($task->description,10,'...')}}</td>
-                                <td>
-                                    <a href="#" class="btn btn-sm btn-primary" title="Ver tarea"><i class="fas fa-eye"></i></a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                @include('partials.datatable.admin.task',['tasks'=>$user->tasks])
             </div>
-
-
-
-
              @include('modals.task.modal-assign-form',['userId'=>$user->id])
-
         </div>
-
-
     </div>
 
+@endsection
 
+@section('js')
+    <!-- Page level plugins -->
+    <script src="{{ asset('js/datatables/jquery.dataTables.js') }}" ></script>
+    <script src="{{ asset('js/datatables/dataTables.bootstrap4.js') }}" ></script>
+
+    <script>
+            // Call the dataTables jQuery plugin
+            $(document).ready(function() {
+                $('#dataTable-staff-user').DataTable();
+                $('#dataTable-tasks').DataTable();
+            });
+    </script>
 
 @endsection
