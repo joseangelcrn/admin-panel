7febf3ba6255cc423b390af981a97ce22e10cc79
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-24 15:52:23 +0100

    active field added

--- database/migrations/2020_12_19_191506_create_tasks_table.php
+++ database/migrations/2020_12_19_191506_create_tasks_table.php
@@ -17,6 +17,7 @@ class CreateTasksTable extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
