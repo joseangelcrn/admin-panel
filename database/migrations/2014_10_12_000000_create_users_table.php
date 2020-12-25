d7280a0d88e378e1f31949643c2ce928b0e3e02f
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-25 19:41:53 +0100

    new fields added to user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('first_surname');
+            $table->string('second_surname');
+            $table->string('user_name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
