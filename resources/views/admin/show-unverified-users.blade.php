29ad75018be519f517e9cf2b37f06cdcd73db207
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-26 12:47:16 +0100

    New differents info views about users

---
+++ resources/views/admin/show-unverified-users.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('title','Admin - Usuarios no verificados')
+
+@section('styles')
+    <link href="{{ asset('js/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+
+    @section('header','Pagina Admin - Usuarios no verificados')
+
+    <div class="container card shadow p-5">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                @include('partials.datatable.admin.user',['users'=>$users])
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('js')
+    <!-- Page level plugins -->
+    <script src="{{ asset('js/datatables/jquery.dataTables.js') }}" ></script>
+    <script src="{{ asset('js/datatables/dataTables.bootstrap4.js') }}" ></script>
+
+    <script>
+            // Call the dataTables jQuery plugin
+            $(document).ready(function() {
+                $('#dataTable-staff-user').DataTable();
+            });
+    </script>
+
+@endsection
---
+++ resources/views/admin/show-users-with-tasks.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('title','Admin - Usuarios con tareas')
+
+@section('styles')
+    <link href="{{ asset('js/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+
+    @section('header','Pagina Admin - Usuarios con tareas')
+
+    <div class="container card shadow p-5">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                @include('partials.datatable.admin.user',['users'=>$users])
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('js')
+    <!-- Page level plugins -->
+    <script src="{{ asset('js/datatables/jquery.dataTables.js') }}" ></script>
+    <script src="{{ asset('js/datatables/dataTables.bootstrap4.js') }}" ></script>
+
+    <script>
+            // Call the dataTables jQuery plugin
+            $(document).ready(function() {
+                $('#dataTable-staff-user').DataTable();
+            });
+    </script>
+
+@endsection
