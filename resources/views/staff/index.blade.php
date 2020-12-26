f8698119106bfea69f3bc7971c8c9f8e9f3eda2d
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-26 17:07:22 +0100

    CommentRefacotring table

--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -26,33 +26,7 @@
     <div class="row">
         <div class="col-lg-12 col-md-12">
            <h1 class="my-5">Tareas asignadas</h1>
-           <table class="table table-bordered" id="dataTable-tasks" width="100%" cellspacing="0">
-               <thead>
-                   <tr>
-                       <th class="col-4">Titulo</th>
-                       <th class="col-4">Descripción</th>
-                       <th class="col-4">Opciones</th>
-                   </tr>
-               </thead>
-               <tfoot>
-                   <tr>
-                        <th class="col-4">Titulo</th>
-                        <th class="col-4">Descripcion</th>
-                        <th class="col-4">Opciones</th>
-                   </tr>
-               </tfoot>
-               <tbody>
-                   @foreach ($tasks  as $task)
-                       <tr>
-                           <td>{{$task->title}}</td>
-                           <td>{{Str::limit($task->description,10,'...')}}</td>
-                           <td>
-                                <a href="#" class="btn btn-sm btn-primary" title="Ver tarea"><i class="fas fa-eye"></i></a>
-                           </td>
-                       </tr>
-                   @endforeach
-               </tbody>
-           </table>
+           @include('partials.datatable.staff.task',['tasks'=>$tasks])
         </div>
     </div>
 </div>
