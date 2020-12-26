da9dac4852bcf11d1e0a866faafa59c76699dd6d
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-26 12:50:08 +0100

    adding links on cards info

--- resources/views/partials/info/admin/task.blade.php
+++ resources/views/partials/info/admin/task.blade.php
@@ -5,54 +5,60 @@
     </div>
     <div class="col-lg-4 col-md-4 my-1">
         <div id="info" class ="card border-left-primary shadow  py-2">
-             <div class="card-body">
-                 <div class="row no-gutters align-items-center">
-                     <div class="col mr-2">
-                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                             Tareas totales
-                         </div>
-                         <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_total']}}</div>
-                     </div>
-                     <div class="col-auto">
-                         <i class="fas fa-info-circle fa-2x"></i>
-                     </div>
-                 </div>
-             </div>
+            <a href="{{route('task.index')}}">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Tareas totales
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_total']}}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-info-circle fa-2x"></i>
+                        </div>
+                    </div>
+                </div>
+            </a>
         </div>
      </div>
      <div class="col-lg-4 col-md-4 my-1">
         <div id="info" class ="card border-left-success shadow  py-2">
-             <div class="card-body">
-                 <div class="row no-gutters align-items-center">
-                     <div class="col mr-2">
-                         <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                             Tareas Activas
-                         </div>
-                         <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_enabled']}}</div>
-                     </div>
-                     <div class="col-auto">
-                         <i class="fas fa-info-circle fa-2x"></i>
-                     </div>
-                 </div>
-             </div>
-        </div>
+            <a href="{{route('task.list-enabled')}}">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Tareas Activas
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_enabled']}}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-info-circle fa-2x"></i>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            </div>
      </div>
      <div class="col-lg-4 col-md-4 my-1">
         <div id="info" class ="card border-left-danger shadow  py-2">
-             <div class="card-body">
-                 <div class="row no-gutters align-items-center">
-                     <div class="col mr-2">
-                         <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                             Tareas no activas
-                         </div>
-                         <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_disabled']}}</div>
-                     </div>
-                     <div class="col-auto">
-                         <i class="fas fa-info-circle fa-2x"></i>
-                     </div>
-                 </div>
-             </div>
-        </div>
+            <a href="{{route('task.list-disabled')}}">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                Tareas no activas
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_disabled']}}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-info-circle fa-2x"></i>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            </div>
      </div>
      <div class="col-lg-6 col-md-6 my-1">
         <div id="info" class ="card border-left-info  shadow  py-2">
