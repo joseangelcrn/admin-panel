e5279a071ba8b9ebadadf439f385745504316647
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-29 12:09:59 +0100

    setting routes on staff bar menu

--- resources/views/modules/menu-bar-left-staff.blade.php
+++ resources/views/modules/menu-bar-left-staff.blade.php
@@ -14,8 +14,8 @@
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Submenu</h6>
-            <a class="collapse-item" href="buttons.html">En proceso</a>
-            <a class="collapse-item" href="buttons.html">Terminadas</a>
+            <a class="collapse-item" href="{{route('staff.tasks-in-progress')}}">En proceso</a>
+            <a class="collapse-item" href="{{route('staff.tasks-completed')}}">Terminadas</a>
         </div>
     </div>
 </li>
