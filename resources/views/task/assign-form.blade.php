01d5e4f62a5548fb9895a1771220992baf9f29ce
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-29 14:16:53 +0100

    assign task form view

---
+++ resources/views/task/assign-form.blade.php
@@ -0,0 +1,176 @@
+@extends('layouts.app')
+
+@section('title','Admin - Asignar tareas a un usuario')
+
+@section('styles')
+    <link rel="tstylesheet" type="text/css" href="{{asset('js/multi-select/dist/css/bootstrap-multiselect.css')}}">
+@endsection
+
+@section('content')
+
+    @section('header','Asignar tareas a un usuario')
+
+    <div class="container card shadow p-5">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <form action="{{route('task.assign')}}" method="POST">
+                    @csrf
+                    @method('POST')
+                    <input type="text" name="user_id" value="{{$user->id}}" style="display: none;">
+                    <div class="form-group">
+                        <select style="display: none;" id="task-selector" class="form-control"  name="task_id[]" multiple="multiple">
+                            @foreach ($tasks as $task)
+                                <option value="{{$task->id}}" data-name="{{$task->name}}" title="{{$task->description}}">{{$task->title}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <button class="btn btn-success">Asignar tareas</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('js')
+    <!-- Page level plugins -->
+    <script type="text/javascript" src="{{asset('js/multi-select/dist/js/bootstrap-multiselect.js')}}"></script>
+
+
+    <script>
+            var task_selector = $('#task-selector');
+
+            // Call the dataTables jQuery plugin
+            $(document).ready(function() {
+                task_selector.multiselect({
+
+                    // allows HTML content
+                    enableHTML: false,
+
+                    // CSS class of the multiselect button
+                    buttonClass: 'custom-select',
+
+                    // inherits the class of the button from the original select
+                    inheritClass: false,
+
+                    // width of the multiselect button
+                    buttonWidth: 'auto',
+
+                    // container that holds both the button as well as the dropdown
+                    buttonContainer: '<div class="btn-group" />',
+
+                    // places the dropdown on the right side
+                    dropRight: false,
+
+                    // places the dropdown on the top
+                    dropUp: false,
+
+                    // CSS class of the selected option
+                    selectedClass: 'active',
+
+                    // maximum height of the dropdown menu
+                    // if maximum height is exceeded a scrollbar will be displayed
+                    maxHeight: false,
+
+                    // includes Select All Option
+                    includeSelectAllOption: true,
+
+                    // shows the Select All Option if options are more than ...
+                    includeSelectAllIfMoreThan: 0,
+
+                    // Lable of Select All
+                    selectAllText: 'Todos los usuarios',
+
+                    // the select all option is added as additional option within the select
+                    // o distinguish this option from the original options the value used for the select all option can be configured using the selectAllValue option.
+                    selectAllValue: 'multiselect-all',
+
+                    // control the name given to the select all option
+                    selectAllName: true,
+
+                    // if true, the number of selected options will be shown in parantheses when all options are seleted.
+                    selectAllNumber: true,
+
+                    // setting both includeSelectAllOption and enableFiltering to true, the select all option does always select only the visible option
+                    // with setting selectAllJustVisible to false this behavior is changed such that always all options (irrespective of whether they are visible) are selected
+                    selectAllJustVisible: true,
+
+                    // enables filtering
+                    enableFiltering: true,
+
+                    // determines if is case sensitive when filtering
+                    enableCaseInsensitiveFiltering: false,
+
+                    // enables full value filtering
+                    enableFullValueFiltering: false,
+
+                    // if true, optgroup's will be clickable, allowing to easily select multiple options belonging to the same group
+                    enableClickableOptGroups: false,
+
+                    // enables collapsible OptGroups
+                    enableCollapsibleOptGroups: false,
+
+                    // collapses all OptGroups on init
+                    collapseOptGroupsByDefault: false,
+
+                    // placeholder of filter filed
+                    filterPlaceholder: 'Search',
+
+                    // possible options: 'text', 'value', 'both'
+                    filterBehavior: 'text',
+
+                    // includes clear button inside the filter filed
+                    includeFilterClearBtn: true,
+
+                    // prevents input change event
+                    preventInputChangeEvent: false,
+
+                    // message to display when no option is selected
+                    nonSelectedText: 'Seleccionar',
+
+                    // message to display if more than numberDisplayed options are selected
+                    nSelectedText: 'selected',
+
+                    // message to display if all options are selected
+                    allSelectedText: 'Todos',
+
+                    // determines if too many options would be displayed
+                    numberDisplayed: 3,
+
+                    // disables the multiselect if empty
+                    disableIfEmpty: false,
+
+                    // message to display if the multiselect is disabled
+                    disabledText: '',
+
+                    // the separator for the list of selected items for mouse-over
+                    delimiterText: ', ',
+
+                    // includes Reset Option
+                    includeResetOption: false,
+
+                    // includes Rest Divider
+                    includeResetDivider: false,
+
+                    // lable of Reset  Option
+                    resetText: 'Reset',
+
+                    // custom templates
+                    templates: {
+                        button: '<button type="button" class="multiselect dropdown-toggle" data-toggle="dropdown"><span class="multiselect-selected-text"></span></button>',
+                        filter: '<div class="multiselect-filter"><div class="input-group input-group-sm p-1"><div class="input-group-prepend"><i class="input-group-text fas fa-search"></i></div><input class="form-control multiselect-search" type="text" /></div></div>',
+                        filterClearBtn: '<div class="input-group-append"><button class="multiselect-clear-filter input-group-text" type="button"><i class="fas fa-times"></i></button></div>',
+                        option: '<button class="multiselect-option dropdown-item"></button>',
+                        divider: '<div class="dropdown-divider"></div>',
+                        optionGroup: '<button class="multiselect-group dropdown-item"></button>',
+                        resetButton: '<div class="multiselect-reset text-center p-2"><button class="btn btn-sm btn-block btn-outline-secondary"></button></div>'
+                    }
+
+                    });
+            });
+    </script>
+
+@endsection
