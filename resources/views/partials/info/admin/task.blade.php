74f55376f50af04d5ac1a781aff121db27977c6c
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-30 03:13:09 +0100

    Mega refactor

--- resources/views/partials/info/admin/task.blade.php
+++ resources/views/partials/info/admin/task.blade.php
@@ -3,147 +3,31 @@
         <h3>Tareas</h3>
         <hr>
     </div>
-    <div class="col-lg-4 col-md-4 my-1">
-        <div id="info" class ="card border-left-primary shadow py-2"
-        title="Todas las tareas del sistema (activas,no activas, asignadas y no asignadas, terminadas y no terminadas)">
-            <a href="{{route('task.index')}}">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Tareas totales
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_total']}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-info-circle fa-2x"></i>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-     </div>
-     <div class="col-lg-4 col-md-4 my-1">
-        <div id="info" class ="card border-left-success shadow  py-2"
-        title="Tareas no cerradas">
-            <a href="{{route('task.list-enabled')}}">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Tareas Activas
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_enabled']}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-info-circle fa-2x"></i>
-                        </div>
-                    </div>
-                </div>
-            </a>
+    <div class="col-lg-12 col-md-12 my-1">
+        @include('partials.components.info-card',[
+            'title'=>reset($taskInfo)['title'],
+            'desc'=>reset($taskInfo)['desc'],
+            'route_name'=>reset($taskInfo)['route_name'],
+            'n'=>reset($taskInfo)['n'],
+            'class'=>reset($taskInfo)['class'],
+            'icon'=>'fas fa-info-circle fa-2x'
+        ])
+    </div>
+    @foreach ($taskInfo as $key=>$info)
+        @if ($key != 'task_total')
+            <div class="col-lg-6 col-md-6  my-1">
+                 @include('partials.components.info-card',[
+                    'title'=>$info['title'],
+                    'desc'=>$info['desc'],
+                    'route_name'=>$info['route_name'],
+                    'n'=>$info['n'],
+                    'n_pc'=>$info['pc'],
+                    'class'=>$info['class'],
+                    'icon'=>'fas fa-info-circle fa-2x'
+                ])
             </div>
-     </div>
-     <div class="col-lg-4 col-md-4 my-1">
-        <div id="info" class ="card border-left-danger shadow  py-2"
-        title="Tareas cerradas">
-            <a href="{{route('task.list-disabled')}}">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                Tareas no activas
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_disabled']}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-info-circle fa-2x"></i>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            </div>
-     </div>
-     <div class="col-lg-6 col-md-6 my-1">
-        <div id="info" class ="card border-left-info  shadow  py-2"
-        title="Tareas asignadas al menos a un usuario">
-            <a href="{{route('task.list-assigned')}}">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-black text-info text-uppercase mb-1">
-                                Tareas asignadas
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_assigned']}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-info-circle fa-2x"></i>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-     </div>
-     <div class="col-lg-6 col-md-6 my-1">
-        <div id="info" class ="card border-left-warning shadow  py-2"
-        title="Tareas sin usuarios asignados">
-            <a href="{{route('task.list-unassigned')}}">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold  text-uppercase mb-1" style="color: #ccaf0c">
-                                Tareas no asignadas
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_not_assigned']}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-info-circle fa-2x"></i>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-     </div>
-     {{-- <div class="separator"></div> --}}
-     <div class="col-lg-6 col-md-6 my-1">
-        <div id="info" class ="card border-left-success shadow  py-2"
-        title="Tareas las cuales los usuarios a los que se le ha asignado han terminado su parte, pendiente de ser verificadas
-        por administradores">
-            <a href="{{route('task.list-completed-unverified')}}">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold  text-uppercase mb-1 text-success">
-                                Tareas pendientes de revisar (completadas)
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_completed']}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-info-circle fa-2x"></i>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-     </div>
+        @endif
+    @endforeach
 
-     <div class="col-lg-6 col-md-6 my-1">
-        <div id="info" class ="card border-left-danger shadow  py-2"
-        title="Tareas en las que, al menos un usuario que la tenga asignada, no ha terminado su parte del trabajo">
-            <a href="{{route('task.list-incompleted')}}">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold  text-uppercase  text-danger mb-1">
-                                Tareas no completas
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$info['task_incompleted']}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-info-circle fa-2x"></i>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-     </div>
 </div>
+
