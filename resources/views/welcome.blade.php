2376713e8f05031369069f7d6df39576f52217d9
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-31 15:11:41 +0100

    welcome view edited

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,25 +3,73 @@
 @section('content')
 
 
-    <div class="container card-shadow mt-5 py-5 bg-gradient-primary text-center rounded">
+    <div class="container card-shadow mt-5 py-5 bg-gradient-primary rounded">
         <div class="row">
             <div class="col-lg-12 col-md-12 text-white text-center">
                 <h1 class="border-bottom">Panel de administración</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, iusto eum. Magnam dolorum doloribus earum architecto! Quod,
-                    labore! Eveniet incidunt asperiores molestias aperiam et! Perspiciatis, in? Mollitia qui quae odit.
-                </p>
             </div>
         </div>
-        <div class="row">
+        <div class="row text-white d-flex justify-content-center">
+            <div class="col-lg-4 col-md-4">
+                <p>Aplicación para la gestión de tareas.</p>
+                <ul>
+                    <li>Roles prediseñados:
+                        <ul>
+                            <li>Admin
+                                <ul>
+                                    <li>Permisos:
+                                       <ul>
+                                            <li>
+                                                Tareas
+                                                <ul>
+                                                    <li>Crear</li>
+                                                    <li>Editar</li>
+                                                    <li>Completar</li>
+                                                    <li>Asignar a usuarios</li>
+                                                    <li>Eliminar</li>
+                                                </ul>
+                                            </li>
+                                            <li>
+                                                Usuarios
+                                                <ul>
+                                                    <li>Editar</li>
+                                                </ul>
+                                            </li>
+                                       </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>Staff
+                                <ul>
+                                    <li>Permisos:
+                                       <ul>
+                                            <li>
+                                                Tareas
+                                                <ul>
+                                                    <li>Completar</li>
+                                                </ul>
+                                            </li>
+                                       </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row text-center">
             <div class="col-lg-12 col-md-12">
                 @auth
+                <a href="
                     @role('admin')
-                        <a href="{{route('admin.index')}}" class="btn btn-primary text-white">
+                        {{route('admin.index')}}
                     @endrole
                     @role('staff')
-                        <a href="{{route('staff.index')}}" class="btn btn-primary text-white">
+                        {{route('staff.index')}}
                     @endrole
+                    "
+                    class="btn btn-primary w-100 pt-4" style="height: 75px;">
                             Mi Panel
                         </a>
                 @endauth
