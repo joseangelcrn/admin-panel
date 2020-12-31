98f35498634b8b26877b83973df657b054288495
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-31 15:16:31 +0100

    fix redirect

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,23 +30,6 @@
                                                     <strong>{{ $errors->first('user_name') ?: $errors->first('email') }}</strong>
                                                 </span>
                                             @endif
-                                            {{-- <input
-                                                type="email"
-                                                class="form-control form-control-user @error('email') is-invalid @enderror"
-                                                id="email"
-                                                aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address..."
-                                                name="email"
-                                                value="{{ old('email') }}"
-                                                required
-                                                autocomplete="email"
-                                                autofocus
-                                                >
-                                                @error('email')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror --}}
                                         </div>
 
 
@@ -79,7 +62,7 @@
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="{{route('register')}}">Create an Account!</a>
                                     </div>
                                 </div>
                             </div>
