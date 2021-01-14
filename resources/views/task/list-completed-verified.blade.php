1d40c4c2dcddc6c2f0d6b50abc0c76aec449fa2d
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2021-01-14 17:11:05 +0100

    view for displaying completed and verified tasks

---
+++ resources/views/task/list-completed-verified.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('title','Admin - Tareas completas (verificadas) ')
+
+@section('styles')
+    <link href="{{ asset('js/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+
+    @section('header','Tareas completas (verificadas) ')
+
+    <div class="container card shadow p-5">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                @include('partials.datatable.task',[
+                    'tasks'=>$tasks,
+                    'options'=>Auth::user()->getOptionsForDataTable(),
+                ])
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('js')
+    <!-- Page level plugins -->
+    <script src="{{ asset('js/datatables/jquery.dataTables.js') }}" ></script>
+    <script src="{{ asset('js/datatables/dataTables.bootstrap4.js') }}" ></script>
+
+    <script>
+            // Call the dataTables jQuery plugin
+            $(document).ready(function() {
+                $('#dataTable-staff-user').DataTable();
+                $('#dataTable-tasks').DataTable();
+            });
+    </script>
+
+@endsection
